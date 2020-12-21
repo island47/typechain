a72e8a15b9f641a8f034011208eb654985aeb601
island47 <island0318@gmail.com>
2020-12-21 13:29:47 +0900

    set typescript

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var name = 'jiuk', age = 24, gender = 'male';
+var sayHi = function (name, age, gender) {
+    console.log("hello " + name + ", you a " + age + " and " + gender);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = 'jiuk',
+	age = 24,
+	gender = 'male';
+
+const sayHi = (name, age, gender) => {
+	console.log(`hello ${name}, you a ${age} and ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
